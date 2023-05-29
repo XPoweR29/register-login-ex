daee47295cc317f1d4cb8531879e98cf3bc0bd81
XPoweR29 <xpower-29@wp.pl>
2023-05-29 16:27:19 +0200

    Error handling setup

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import { handleError } from './utils/errors';
 
 export const app = express();
 
 app.use(express.json());
+
+
+app.use(handleError);
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on http://localhost:3001');
