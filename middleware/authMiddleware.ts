d650848f190b9275c31d3918aca03b6f8f6139b1
XPoweR29 <xpower-29@wp.pl>
2023-05-31 16:52:35 +0200

    Correct authMiddleware

--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -7,13 +7,19 @@ dotenv.config();
 export const authMiddleware = (req: Record<'user', any> & Request, res: Response, next: NextFunction) => {
     const headerToken = req.headers['authorization'];
     if(!headerToken) {
-        return res.status(401);
+        return res.status(401).json({
+            isSuccess: false,
+            message: 'Authorization token not found'
+        });
     }
 
     const token = headerToken.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
         if(err) {
-            return res.status(403);
+            return res.status(403).json({
+                isSuccess: false,
+                message: 'Invalid access token'
+            });
         }
 
         req.user = decoded;
