d07c81f6f6158e6dfc921209f0cb702f7d0a9f07
XPoweR29 <xpower-29@wp.pl>
2023-05-31 16:03:32 +0200

    Upgrade login EP and set authMiddleware for userRouter

--- routers/user.router.ts
+++ routers/user.router.ts
@@ -1,13 +1,41 @@
 import { Router } from "express";
 import { UserRecord } from "../records/user.record";
+import * as jwt from 'jsonwebtoken';
+import { User } from "../types";
+import * as dotenv from 'dotenv';
+import { compare } from "bcrypt";
+import { authMiddleware } from "../middleware/authMiddleware";
+
+dotenv.config();
+
 
 export const userRouter = Router();
 
 userRouter
 
-.post('/login', (req, res) => {
-    console.log(req.body);
-    res.json({isSucces: true}); 
+.post('/login', authMiddleware, async(req, res) => {
+    const user = await UserRecord.getUserByEmail(req.body.email);
+    if(!user){
+        return res.status(401).json({
+            isSuccess: false,
+            message: 'User not found'
+        });
+    }
+
+    if(!(await compare(req.body.pwd, user.pwd))) {
+        return res.status(401).json({
+            isSucess: false,
+            message: 'Invalid password'
+        });
+    }
+
+    const payload: Partial<User> = {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+    }
+    const token = jwt.sign(payload, process.env.ACCESS_TOKEN, {expiresIn: '15s'});
+    res.json({token});
 })
 
 .post('/register', (req, res) => {
